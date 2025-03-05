a232e91ce422ea6698e606f95c62256ac6c4fbb3
ioannispechlivanis <pechlivanis.i17@gmail.com>
2025-03-05 14:21:08 +0100

    refactor Tips

---
+++ TipKitTutorial/TipKitTutorial/Tips/AddContactTip.swift
@@ -0,0 +1,8 @@
+//
+//  AddContactTip.swift
+//  TipKitTutorial
+//
+//  Created by Ioannis Pechlivanis on 05.03.25.
+//
+
+import Foundation
---
+++ TipKitTutorial/TipKitTutorial/Tips/DeleteTip.swift
@@ -0,0 +1,8 @@
+//
+//  DeleteTip.swift
+//  TipKitTutorial
+//
+//  Created by Ioannis Pechlivanis on 05.03.25.
+//
+
+import Foundation
